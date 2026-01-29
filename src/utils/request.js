150a4ecce3cf78db653589f3bfd7168b6bc295cb
FangJie <2587618709@qq.com>
2026-01-29 10:08:05 +0800

    ws_base1

--- src/utils/request.js
+++ src/utils/request.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 // src/config/server.js
 export const HTTP_BASE = `/api`
-export const WS_BASE   = `ws://localhost:8080/ws/telemetry`
+export const WS_BASE   = `ws://192.168.246.214:8080/ws/telemetry`
 
 const http = axios.create({
     baseURL:HTTP_BASE,
